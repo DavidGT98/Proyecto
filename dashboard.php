8e832731faa184a239e14d3e22c6a832412df651
DavidGT98 <56262968+DavidGT98@users.noreply.github.com>
2020-11-08 20:52:54 +0100

    Commit inicial

---
+++ dashboard.php
@@ -0,0 +1,73 @@
+<?php
+session_start();
+if (!isset($_SESSION['usuario'])) {
+    header("location:index.php");
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+    <!-- Google Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
+    <!-- JQuery -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js"></script>
+    <title>Dashboard</title>
+</head>
+
+<body>
+<!--Navbar -->
+<nav class="mb-1 navbar navbar-expand-lg navbar-light default-color lighten-1">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-555"
+    aria-controls="navbarSupportedContent-555" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent-555">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Inicio
+          <span class="sr-only">(current)</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Movimientos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+    </ul>
+    <ul class="navbar-nav ml-auto nav-flex-icons">
+      <li class="nav-item avatar">
+        <a class="nav-link p-0" href="#">
+          <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-5.jpg" class="rounded-circle z-depth-0"
+            alt="avatar image" height="35">
+        </a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<!--/.Navbar -->
+    <!-- Collapsible content -->
+    <h1>Has accedido a la sesion</h1>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,98 @@
+<html>
+
+<head>
+    <!--JQUERY-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <!-- FRAMEWORK BOOTSTRAP para el estilo de la pagina-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+    <!-- Los iconos tipo Solid de Fontawesome-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/solid.css">
+    <script src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
+
+    <!-- Nuestro css-->
+    <link rel="stylesheet" type="text/css" href="src/css/css.css">
+
+    <title>Login Form</title>
+    <?php
+
+    require_once("libreria.php");
+    ?>
+
+</head>
+
+<body>
+    <div class="modal-dialog text-center">
+        <div class="col-sm-8 main-section">
+            <div class="modal-content">
+                <div class="col-12 user-img">
+                    <img src="src/img/user.png" alt="user">
+                </div>
+                <form name=f1 class="col-12" action="#" method="POST">
+                    <div class="form-group">
+                        <p class="icono-login">
+                            <input name="user" type="text" class="form-control" placeholder="Nombre de usuario" id="user-group">
+                        </p>
+                    </div>
+                    <div class="form-group">
+                        <p class="icono-password">
+                            <input name="pass" type="password" class="form-control" placeholder="Constraseña" id="pass-group">
+                        </p>
+                    </div>
+                    <!--                     <button name="Enviar" value="Enviar" type="button" class="btn btn-warning">Login</button> -->
+                    <input type=submit name="Enviar" value="Enviar" class="btn btn-warning">
+                    <div class="col-12 forgot">
+                        <a href="#" class="badge badge-info">Contraseña olvidada</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <?php
+    $salt1 = "#$.-6j";
+    $salt2 = "?[*-+¿";
+
+    function LoginCorrecto($usu, $cla)
+    {
+        $consulta = "select count(*) as Cuenta 
+             from usuarios 
+			 where nombre='$usu' and clave='$cla'";
+
+        $db = Conectar("proyecto");
+
+        $datos = ConsultaDatos($db, $consulta);
+
+        $fila = $datos[0];
+
+        $cuenta = $fila['Cuenta'];
+
+        Cerrar($db);
+
+        return $cuenta;   // Puede ser 0 o 1 que sirve como TRUE o FALSE
+    }
+
+    if (isset($_POST['Enviar'])) {
+        $usuario = $_POST['user'];
+        $clave = $_POST['pass'];
+        $clave = sha1($salt1 . $clave . $salt2);
+
+        if (LoginCorrecto($usuario, $clave)) {
+            echo "Login corecto";
+            $_SESSION['usuario'] = $usu;    //Creamos una de sesion para ese usuario 
+            header("location: dashboard.php");  //Redireccionamos la pagina del menu
+        } else {
+            echo "<script>alert('Usuario/clave incorrecto');</script>";
+            echo "<div class='alert alert-warning'>Login incorrecto</div>";
+        }
+    }
+
+
+    ?>
+
+
+</body>
+
+</html>
